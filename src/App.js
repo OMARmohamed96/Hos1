bb2e8dbffdf99e9d1cc46f1e5722718ea3a364fe
OMARmohamed96 <32283515+OMARmohamed96@users.noreply.github.com>
2018-12-13 13:51:05 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom' ;
+import {Route, Link, NavLink} from 'react-router-dom';
+import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/SignInForm';
+import logo from './logo.png';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+       <div className="App__Aside"><img src={logo} alt="app_logo" />;</div>
+       <div className="App__Form">
+       <div className = "PageSwitcher">
+       <NavLink to ="/SignIn"  activeClassName="PageSwitcher__Item--Active" className ="PageSwitcher__Item ">Sign In</NavLink>
+       <NavLink exact to ="/" activeClassName="PageSwitcher__Item--Active" className ="PageSwitcher__Item ">Sign Up</NavLink>
+       </div>
+       <div className="FormTitle">
+       <NavLink to="/SignIn" activeClassName="FormTitle__Link--Active" className ="FormTitle__Link ">Sign In
+       </NavLink> or <NavLink exact to= "/" activeClassName="FormTitle__Link--Active"  className ="FormTitle__Link  ">Sign Up</NavLink>
+       </div>
+
+      <Route exact path="/" component={SignUpForm} >
+       </Route>
+       <Route path="/SignIn" component={SignInForm}>
+       </Route>
+
+       </div>
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
